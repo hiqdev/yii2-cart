9ded1e80414464329036ef7f3822130f4ae765d1
Andrii Vasyliev <sol@hiqdev.com>
2015-11-16 11:39:16 +0000

    php-cs-fixed

--- src/CartPositionInterface.php
+++ src/CartPositionInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Cart module for yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-cart
+ * @package   yii2-cart
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
 namespace hiqdev\yii2\cart;
 
 /**
