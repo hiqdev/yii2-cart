8684813c41745a4c346ef192fb02a5066a7a7bb5
Andrey Klochok <andreyklochok@gmail.com>
2015-11-19 14:36:03 +0200

    Replace $model->quantity to $model->getQuantity() !?!?!?

--- src/widgets/views/QuantityCell_view.php
+++ src/widgets/views/QuantityCell_view.php
@@ -1,16 +1,16 @@
 <?php
 
 use yii\helpers\Html;
-
+\yii\helpers\VarDumper::dump($model->quantity, 10, true);
 ?>
 
 <?= Html::beginForm('update-quantity', 'post', ['id' => $model->id]) ?>
 
     <?= Html::hiddenInput('id', $model->id) ?>
     <?php if ($widget->isSelectMode()) : ?>
-        <?= Html::dropDownList('quantity', $model->quantity, $model->getQuantityOptions(), ['class' => 'form-control quantity-field']) ?>
+        <?= Html::dropDownList('quantity', $model->getQuantity(), $model->getQuantityOptions(), ['class' => 'form-control quantity-field']) ?>
     <?php else : ?>
-        <?= Html::input('number', 'quantity', $model->quantity, ['class' => 'form-control quantity-field', 'min' => 1, 'max' => 99, 'step' => 1]) ?>
+        <?= Html::input('number', 'quantity', $model->getQuantity(), ['class' => 'form-control quantity-field', 'min' => 1, 'max' => 99, 'step' => 1]) ?>
     <?php endif ?>
 
 <?= Html::endForm() ?>
