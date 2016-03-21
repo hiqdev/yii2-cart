32a44713f4dff9f6153c6363724dc9a676500cfb
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-03-21 19:24:37 +0200

    Updated QuantityCell view to render result of Position::getQuantityOptions() if value is a string

--- src/widgets/views/QuantityCell_view.php
+++ src/widgets/views/QuantityCell_view.php
@@ -6,11 +6,17 @@ use yii\helpers\Html;
 
 <?= Html::beginForm('update-quantity', 'post', ['id' => $model->id]) ?>
 
-    <?= Html::hiddenInput('id', $model->id) ?>
+<?= Html::hiddenInput('id', $model->id) ?>
+<?php $quantityOptions = $model->getQuantityOptions(); ?>
+<?php if (is_array($quantityOptions)) : ?>
     <?php if ($widget->isSelectMode()) : ?>
-        <?= Html::dropDownList('quantity', $model->getQuantity(), $model->getQuantityOptions(), ['class' => 'form-control quantity-field']) ?>
+        <?= Html::dropDownList('quantity', $model->getQuantity(), $quantityOptions, ['class' => 'form-control quantity-field']) ?>
     <?php else : ?>
-        <?= Html::input('number', 'quantity', $model->getQuantity(), ['class' => 'form-control quantity-field', 'min' => 1, 'max' => 99, 'step' => 1]) ?>
+        <?= Html::input('number', 'quantity', $model->getQuantity(),
+            ['class' => 'form-control quantity-field', 'min' => 1, 'max' => 99, 'step' => 1]) ?>
     <?php endif ?>
+    <?php else : ?>
+        <?= $quantityOptions ?>
+<?php endif ?>
 
 <?= Html::endForm() ?>
