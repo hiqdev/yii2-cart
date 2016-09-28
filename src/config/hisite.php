7fa6cb2cb2d89eee8e7aa052477f88bc1534c259
Andrii Vasyliev <sol@hiqdev.com>
2016-09-28 14:20:37 +0000

    + themeManager config for theming widgets

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    'components' => [
+        'themeManager' => [
+            'widgetPaths' => [
+                'cart' => '@hiqdev/yii2/cart/widgets/views',
+            ],
+        ],
+    ],
     'modules' => [
         'cart' => [
             'class' => \hiqdev\yii2\cart\Module::class,
