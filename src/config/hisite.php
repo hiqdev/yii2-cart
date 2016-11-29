47b6bbbe78cd6abae8d21f7482752bcbb71486a9
Andrii Vasyliev <sol@hiqdev.com>
2016-11-29 16:17:17 +0000

    + i18n config

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -8,6 +8,15 @@ return [
                 '@hiqdev/yii2/cart/views' => '$themedViewPaths',
             ],
         ],
+        'i18n' => [
+            'translations' => [
+                'cart' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'sourceLanguage' => 'en-US',
+                    'basePath' => '@hiqdev/yii2/cart/messages',
+                ],
+            ],
+        ],
     ],
     'modules' => [
         'cart' => [
