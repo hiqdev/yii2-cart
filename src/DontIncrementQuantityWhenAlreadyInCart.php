22056ac894f7d0d02b7bed84a1ebc01a5f503b22
Yurii Myronchuk <bladeroot@gmail.com>
2018-05-10 15:27:10 +0000

    change annotation

--- src/DontIncrementQuantityWhenAlreadyInCart.php
+++ src/DontIncrementQuantityWhenAlreadyInCart.php
@@ -4,7 +4,7 @@ namespace hiqdev\yii2\cart;
 
 /**
  * Should be used to prevent position quantity modification
- * Interface ImmutableQuantityInterface
+ * Interface DontIncrementQuantityWhenAlreadyInCart
  * @package hiqdev\yii2\cart
  */
 interface DontIncrementQuantityWhenAlreadyInCart
