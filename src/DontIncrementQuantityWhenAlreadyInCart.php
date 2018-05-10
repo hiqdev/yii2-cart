3ce1a9627c124a5e1c584c5f99a6466ccce66cb8
Yurii Myronchuk <bladeroot@gmail.com>
2018-05-10 15:25:45 +0000

    add DontIncrementQuantityWhenAlreadyInCart

---
+++ src/DontIncrementQuantityWhenAlreadyInCart.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\yii2\cart;
+
+/**
+ * Should be used to prevent position quantity modification
+ * Interface ImmutableQuantityInterface
+ * @package hiqdev\yii2\cart
+ */
+interface DontIncrementQuantityWhenAlreadyInCart
+{
+}
