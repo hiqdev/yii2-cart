b01b88f350f12222b2d6c15ebc2b6ee83b0fac50
pkolomiy <pkolomiy@gmail.com>
2018-08-14 18:12:27 +0300

    Changed path to CheckboxStyleAsset

--- src/views/cart/index.php
+++ src/views/cart/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use hipanel\assets\CheckboxStyleAsset;
+use hiqdev\themes\adminlte\CheckboxStyleAsset;
 use hiqdev\yii2\cart\widgets\QuantityCell;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
